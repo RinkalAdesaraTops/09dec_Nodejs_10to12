d09ef1ae68359f638327829472e16bdca9c8be6c
Rinkal <rinkaladesara.tops@gmail.com>
2025-04-28 11:28:40 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,9 @@
+const add = (a,b)=>{
+    console.log("Addition is "+(a+b))
+}
+
+const minus = (a,b)=>{
+    console.log("Minus is "+(a-b))
+}
+// module.exports = add
+module.exports = {add,minus}
