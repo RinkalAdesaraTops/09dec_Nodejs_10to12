6bb08668ed3d121906530125aeefae06179fb2c2
Rinkal <rinkaladesara.tops@gmail.com>
2025-05-02 10:12:24 +0530

    nodejs

---
+++ index copy.js
@@ -0,0 +1,16 @@
+//core module
+const {add,minus} = require('./calc')
+const http = require('http')
+http.createServer((req,res)=>{
+    // res.sendFile("Hellooo")
+    
+    res.write("Hello 123ws wertuyr weruw")
+    add(45,20)
+    minus(10,4)
+    // res.write("<br> Addition is "+add(20,5))
+    // res.write("<br> Minus is "+minus(20,10))
+    res.end("end..")
+}).listen(4000,()=>{
+    console.log("Listening on 4000 port...")
+})
+
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-//core module
-const {add,minus} = require('./calc')
-const http = require('http')
-http.createServer((req,res)=>{
-    // res.sendFile("Hellooo")
-    res.write("Hello 123ws wertuyr weruw")
-    add(45,20)
-    minus(10,4)
-    // res.write("<br> Addition is "+add(20,5))
-    // res.write("<br> Minus is "+minus(20,10))
-    res.end("end..")
-}).listen(4000,()=>{
-    console.log("Listening on 4000 port...")
+var express = require('express')
+var app = express()
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.send("Hellooo")
 })
-
+app.get('/home',(req,res)=>{
+    // res.send("My Home page")
+    // console.log(__dirname);
+    // res.sendFile(__dirname+"/home.html")
+    res.render('first')
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname+"/about.html")
+})
+app.listen(4000,()=>{
+    console.log("Running 4000 port");  
+})
